a1241ef6b5050bcd6a01ca5848fcd6f767b54249
baborges45 <brunoquemsoueu@gmail.com>
2021-12-21 23:06:47 -0300

    Creating the progressBar and adjusting the bar

--- lib/screens/home/category_item.dart
+++ lib/screens/home/category_item.dart
@@ -10,26 +10,70 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 220,
-        padding: EdgeInsets.symmetric(
-            horizontal: k.defaulPadding, vertical: k.defaulPadding),
-        decoration: BoxDecoration(
-            color: Vx.white,
-            borderRadius: BorderRadius.circular(24),
+      width: 220,
+      padding: EdgeInsets.symmetric(
+          horizontal: k.defaulPadding, vertical: k.defaulPadding),
+      decoration: BoxDecoration(
+          color: Vx.white,
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 10),
+              blurRadius: 10,
+              spreadRadius: -5,
+              color: Vx.gray500.withOpacity(0.1),
+            ),
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          '40 tasks'.text.color(k.defaultGray).make(),
+          'Business'.text.xl2.bold.make(),
+          ProgressBar(color: Vx.purple300),
+        ],
+      ),
+    );
+  }
+}
+
+class ProgressBar extends StatelessWidget {
+  final Color color;
+  const ProgressBar({
+    Key? key,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 4,
+          width: 172,
+          decoration: BoxDecoration(
+            color: Vx.blueGray200,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+        AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          height: 4,
+          width: 80,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(4),
             boxShadow: [
               BoxShadow(
-                offset: Offset(0, 10),
-                blurRadius: 10,
+                offset: Offset(0, 2),
+                blurRadius: 3,
                 spreadRadius: -5,
-                color: Vx.gray500.withOpacity(0.1),
+                color: Vx.blue500.withOpacity(0.25),
               ),
-            ]),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            '40 tasks'.text.color(k.defaultGray).make(),
-            'Business'.text.xl2.bold.make(),
-          ],
-        ));
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
