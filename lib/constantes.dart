ac5d20a5795d1d9692c61c39f9ed32813966d476
baborges45 <brunoquemsoueu@gmail.com>
2021-12-28 02:40:49 -0300

    create animated sidebar

--- lib/constantes.dart
+++ lib/constantes.dart
@@ -4,5 +4,7 @@ import 'package:velocity_x/velocity_x.dart';
 class k {
   static double iconSize = 30;
   static Color defaultGray = Vx.blueGray400;
+  static Color defaultBgColor = Vx.blueGray50;
+  static Color primaryColor = Color(0xFF283B71);
   static const double defaulPadding = 30.0;
 }
--- lib/screens/home/components/home_screen.dart
+++ lib/screens/home/components/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart' hide Title;
 import 'package:todo_list/screens/home/components/category_list.dart';
 import 'package:todo_list/shared/components/subtitle.dart';
@@ -8,11 +10,17 @@ import 'package:velocity_x/velocity_x.dart';
 
 import 'task_list.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   //const Home({Key? key}) : super(key: key);
   //Home createState() => Home();
   // List<Task> tasks = [];
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   bool isMenuOpen = false;
+  double pageScale = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -21,45 +29,65 @@ class Home extends StatelessWidget {
       body: Stack(
         clipBehavior: Clip.none,
         children: [
-          AnimatedPositioned(
-            duration: Duration(milliseconds: 750),
-            curve: Curves.easeInOut,
-            width: size.width,
-            height: size.height,
-            left: isMenuOpen ? 350 : 0,
-            child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TopBar(
-                    onToggleMenu: () {
-                      setState(() {
-                        this.isMenuOpen = !this.isMenuOpen;
-                      });
-                    },
-                  ),
-                  Title(
-                    text: "What's up,Joy!",
-                  ),
-                  Subtitle(text: 'Categories'),
-
-                  ///foi criado os box e colocados em um listview horizontal
-                  CategoryList(),
-                  SizedBox(height: k.defaulPadding),
-                  Subtitle(text: 'Today\'s Tasks'),
-                  SizedBox(height: k.defaulPadding / 2),
-
-                  TaskList()
-                ],
-              ),
-            )
-                .box
-                .border(color: Vx.red500, width: 5)
-                .withRounded(value: 5)
-                .make(),
-          ),
+          buildMenu(size),
+          buildPage(size),
         ],
       ),
+    );
+  }
+
+  Widget buildMenu(Size size) {
+    return SafeArea(
+      child: Container(
+        child: Container(
+          width: 100,
+          child: Center(
+            child: Text(
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+              'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,'
+              'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ).box.color(k.primaryColor).width(size.width).height(size.height).make();
+  }
+
+  AnimatedPositioned buildPage(Size size) {
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 750),
+      curve: Curves.easeInOut,
+      width: size.width,
+      height: size.height,
+      left: isMenuOpen ? 330 : 0,
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TopBar(
+              onToggleMenu: () {
+                setState(() {
+                  this.isMenuOpen = !this.isMenuOpen;
+                  pageScale = isMenuOpen ? 0.7 : 1;
+                });
+              },
+            ),
+            Title(
+              text: "What's up,Joy!",
+            ),
+            Subtitle(text: 'Categories'),
+
+            ///foi criado os box e colocados em um listview horizontal
+            CategoryList(),
+            SizedBox(height: k.defaulPadding),
+            Subtitle(text: 'Today\'s Tasks'),
+            SizedBox(height: k.defaulPadding / 2),
+
+            TaskList()
+          ],
+        ),
+      ).animatedBox.color(k.defaultBgColor).withRounded(value: 32).make(),
     );
   }
 }
